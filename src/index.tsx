244bfdc9a17958f4a99c78e20a7db672fb713aff
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-04-19 13:44:42 -0400

    turn board into fc

--- src/index.tsx
+++ src/index.tsx
@@ -11,35 +11,32 @@ function Square(): JSX.Element {
     );
 }
 
-class Board extends React.Component {
-    renderSquare(i: any) {
+function Board(): JSX.Element {
+    function renderSquare(i: any) {
         return <Square />;
     }
+    const status = 'Next player: X';
 
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-        );
-    }
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
 }
 
 class Game extends React.Component {
